b9162d72497b3be3f739032aa5ff89bd8b9a2694
Nandi Wong <nandi0315@gmail.com>
2020-03-04 17:52:38 +0800

    use callback for mkdirp instead

--- build.js
+++ build.js
@@ -1,9 +1,16 @@
 const mkdirp = require('mkdirp');
 const fs = require('fs');
 
-mkdirp('./public')
+mkdirp('./public',  (err) => {
+  if (err) {
+    console.error(err);
+    throw (error)
+  }
+  
+  let content = fs.readFileSync('web/index.html').toString();
+  content = content.replace('Anonymous', process.env.USERNAME || 'Nobody')
 
-let content = fs.readFileSync('web/index.html').toString();
-content = content.replace('Anonymous', process.env.USERNAME || 'Nobody')
+  fs.writeFileSync('public/index.html', content);
 
-fs.writeFileSync('public/index.html', content);
+})
+
